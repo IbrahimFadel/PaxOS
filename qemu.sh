7172314c6a1b4ad1f1e5217ffba16e48f0e13259
Ibrahim Fadel <ibrahim.m.fadel@gmail.com>
2025-01-02 20:05:38 -0500

    update README with instructions

--- qemu.sh
+++ qemu.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+cd kernel
+meson compile -C build-i386
+
 HOST=i386
 DEBUG_FLAGS="-s -S"
-FLAGS="-cdrom pax_os.iso -serial stdio"
+FLAGS="-cdrom build-i386/pax_os.iso -serial stdio"
 
 if [[ $1 == "debug" ]]; then
 	FLAGS="$FLAGS $DEBUG_FLAGS"
